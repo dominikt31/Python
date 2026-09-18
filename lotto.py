32dc49ebdd9517035492856e942117f41ebeec10
dominikt31 <dominik.tkocz31@gmail.com>
2026-09-18 15:15:44 +0200

    losowanie dziala

--- lotto.py
+++ lotto.py
@@ -4,6 +4,7 @@ class Lotto:
     def __init__(self):
         self.strzal = []
         self.wylosowane = []
+        self.ile_liczb = 6
         self.licznik = 0
 
     def posortuj(self, tablica):
@@ -15,21 +16,37 @@ class Lotto:
                     tablica[j] = tablica[j+1]
                     tablica[j+1] = temp
 
-    def wylosuj(self, tablica):
-        for _ in range(0,6):
+    def wylosuj(self):
+        tablica = []
+        for _ in range(0, self.ile_liczb):
             tablica.append(random.randint(1,49))
         self.posortuj(tablica)
-        for i in range (0,5):
+        for i in range (0, self.ile_liczb-1):
             if tablica[i] == tablica[i+1]:
-                tablica = []
-                self.wylosuj(tablica)
-            print(tablica)
+                tablica = self.wylosuj()
+        return tablica
+
+    def czy_wygrana(self):
+        wygrano = True
+        for i in range(0, self.ile_liczb):
+            if(self.strzal[i] != self.wylosowane[i]):
+                wygrano = False
+        if wygrano:
+            print(f'Wygrano po {self.licznik} próbach\nWylosowane liczby:{self.strzal}')
+            return True
+        else:
+            self.licznik += 1
 
     def zagraj_automatycznie(self):
-        self.wylosuj(self.strzal)
+        wygrano = False
+        while not wygrano:
+            self.strzal = self.wylosuj()
+            self.wylosowane = self.wylosuj()
+            wygrano = self.czy_wygrana()
 
 def main():
     los = Lotto()
+    los.ile_liczb = 5
     los.zagraj_automatycznie()
 
 if __name__ == "__main__":
